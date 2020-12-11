a00f4e792c4875f49550bf03c03dcc402fe2f208
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-11 11:42:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+class Cat{
+   constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  } 
+    speak() {
+    let cat = new Cat("Sasha", "female");
+    return cat.name + " says meow!";
+  }
+}
+
+
+
+class Dog{
+    constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  
+   speak() {
+    let dog = new Dog("Rufio", "male");
+    return dog.name + " says woof!";
+  }
+}
+
+class Bird{
+    constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  
+  speak() {
+    if(this.sex==="male"){
+    return `It's me! ${this.name}, the parrot!`;
+  }else{
+    return `${this.name} says squawk!`;
+  }
+}
+}
+
+
+
